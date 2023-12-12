e35ea7a0ad66f25eb0584578c2e4bdd9edd63c00
YlanNaly <hei.nalisoa@gmail.com>
2023-12-12 21:03:14 +0300

    feat: structure of project

---
+++ exception-handler/src/main/java/com/exceptionhandler/controller/FileController.java
@@ -0,0 +1,12 @@
+package com.exceptionhandler.controller;
+
+import lombok.AllArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@AllArgsConstructor
+@RestController
+@RequestMapping("upload")
+public class FileController {
+    // TODO: implement controller
+}
---
+++ exception-handler/src/main/java/com/exceptionhandler/service/FileService.java
@@ -0,0 +1,8 @@
+package com.exceptionhandler.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class FileService {
+    // TODO : implement service
+}
