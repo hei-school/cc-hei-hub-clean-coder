93f280a611f7303d0c59911b777cf51d47142fc0
YlanNaly <hei.nalisoa@gmail.com>
2023-12-12 21:48:38 +0300

    feat: implement interface storagefile

--- exception-handler/src/main/java/com/exceptionhandler/service/FileService.java
+++ exception-handler/src/main/java/com/exceptionhandler/service/FileService.java
@@ -1,8 +1,47 @@
 package com.exceptionhandler.service;
 
+import com.exceptionhandler.service.validator.SizeValidator;
+import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.stream.Stream;
 
 @Service
-public class FileService {
-    // TODO : implement service
+@AllArgsConstructor
+public class FileService implements StorageFileInterface{
+    private final SizeValidator validator;
+    private static final String LOCATION = "../../../files";
+    private final Path rootLocation = Paths.get(LOCATION);
+    @Override
+    public String store(MultipartFile file) {
+        return null;
+    }
+
+    @Override
+    public Stream<Path> loadFile() {
+        return null;
+    }
+
+    @Override
+    public String deleteFile() {
+        return null;
+    }
+
+    @Override
+    public Path load(String filename) {
+        return null;
+    }
+
+    @Override
+    public String getFileFormat(MultipartFile file) {
+        return null;
+    }
+
+    @Override
+    public String getFileExtension(MultipartFile file) {
+        return null;
+    }
 }
