108d1b977e08f0fd350324c7232c81c1f31c416e
YlanNaly <hei.nalisoa@gmail.com>
2023-12-13 11:12:21 +0300

    feat: post method to store file from multipart form

--- exception-handler/src/main/java/com/exceptionhandler/controller/FileController.java
+++ exception-handler/src/main/java/com/exceptionhandler/controller/FileController.java
@@ -1,12 +1,20 @@
 package com.exceptionhandler.controller;
 
+import com.exceptionhandler.service.FileService;
 import lombok.AllArgsConstructor;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.util.concurrent.TimeoutException;
 
 @AllArgsConstructor
 @RestController
-@RequestMapping("upload")
 public class FileController {
-    // TODO: implement controller
+    private final FileService service;
+    @PostMapping("/upload")
+    public String storeFile (
+            @RequestPart("file") MultipartFile file
+            ) throws TimeoutException {
+        return service.store(file);
+    }
 }
