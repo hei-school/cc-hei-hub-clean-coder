45bf9d0165c27f1b9f62c04af91a7ae54ead0c83
YlanNaly <hei.nalisoa@gmail.com>
2023-12-13 11:10:47 +0300

    feat: throws error when error occured

--- exception-handler/src/main/java/com/exceptionhandler/service/StorageFileInterface.java
+++ exception-handler/src/main/java/com/exceptionhandler/service/StorageFileInterface.java
@@ -3,10 +3,11 @@ package com.exceptionhandler.service;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.nio.file.Path;
+import java.util.concurrent.TimeoutException;
 import java.util.stream.Stream;
 
 public interface StorageFileInterface {
-    String store(MultipartFile file);
+    String store(MultipartFile file) throws TimeoutException;
     Stream<Path> loadFile();
     String deleteFile();
     Path load(String filename);
