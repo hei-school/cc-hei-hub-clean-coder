271f0afd054479cfde7dee0ffbeafa3746a68820
YlanNaly <hei.nalisoa@gmail.com>
2023-12-13 18:00:25 +0300

    feat: now we can upload file without specifying type on endpoint

--- exception-handler/src/main/java/com/exceptionhandler/controller/FileController.java
+++ exception-handler/src/main/java/com/exceptionhandler/controller/FileController.java
@@ -2,16 +2,40 @@ package com.exceptionhandler.controller;
 
 import com.exceptionhandler.service.FileService;
 import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 import java.nio.file.Path;
 
 import java.util.concurrent.TimeoutException;
+import java.util.stream.Stream;
 
 @AllArgsConstructor
 @RestController
 public class FileController {
     private final FileService service;
+
+    @PostMapping("/upload")
+    public ResponseEntity<String> uploadFile(
+            @RequestParam("fileType") String fileType,
+            @RequestPart("file") MultipartFile file
+    ) {
+        try {
+            String responseMessage = "";
+            switch (fileType) {
+                case "document" -> responseMessage = service.storeDocument(file);
+                case "video" -> responseMessage = service.storeVideo(file);
+                case "image" -> responseMessage = service.storeImage(file);
+                default -> {
+                    return ResponseEntity.badRequest().body("Invalid file type provided.");
+                }
+            }
+            return ResponseEntity.ok(responseMessage);
+        } catch (TimeoutException e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Timeout exception occurred.");
+        }
+    }
     
     @PostMapping("/upload-document")
     public String storeDocument (
