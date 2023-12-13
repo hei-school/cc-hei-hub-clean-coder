63e17f5aa3c00138482ce42272127f178fb38adc
soamariaka <hei.soa.2@gmail.com>
2023-12-13 13:25:05 +0300

    feat: add handling for DuplicatedFile exception

--- services/uploadHandler.js
+++ services/uploadHandler.js
@@ -4,6 +4,7 @@ import fs from 'fs/promises';
 
 import BadFileType from './exceptions/BadFileType.js';
 import CorruptedFile from './exceptions/CorruptedFile.js';
+import DuplicatedFile from './exceptions/DuplicatedFile.js';
 
 export const handleUpload = async (file, directory) => {
   try {
@@ -23,11 +24,19 @@ export const handleUpload = async (file, directory) => {
 
     const filename = `${Date.now()}.${type.ext}`;
     const filePath = path.join(directory, filename);
+
+    const files = await fs.readdir(directory);
+    const isDuplicated = files.some((existingFile) => path.parse(existingFile).name === path.parse(filePath).name);
+
+    if (isDuplicated) {
+      throw new DuplicatedFile('File with the same name already exists');
+    }
+
     await fs.writeFile(filePath, file);
     return filePath;
   } catch (error) {
-    if (error instanceof BadFileType || error instanceof CorruptedFile) {
-      throw error; 
+    if (error instanceof BadFileType || error instanceof CorruptedFile || error instanceof DuplicatedFile) {
+      throw error;
     } else {
       console.error(error);
       throw new Error('Internal server error');
@@ -40,12 +49,12 @@ const isFileCorrupted = (file) => {
     const fileText = file.toString('utf-8');
 
     if (fileText.includes('CorruptedString')) {
-      return true; 
+      return true;
     }
 
     return false;
   } catch (error) {
     console.error('Error during file corruption check:', error);
-    return true; 
+    return true;
   }
 };
